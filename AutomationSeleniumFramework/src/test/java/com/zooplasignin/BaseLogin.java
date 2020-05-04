99bd9ccd16817373c9c778bd2e56fee212a2dfde
Administrator <Administrator@192.168.0.23>
2020-05-04 18:23:34 -0400

    Update 5-4-2020

--- AutomationSeleniumFramework/src/test/java/com/zooplasignin/BaseLogin.java
+++ AutomationSeleniumFramework/src/test/java/com/zooplasignin/BaseLogin.java
@@ -1,7 +1,10 @@
 package com.zooplasignin;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BaseLogin {
 
@@ -15,9 +18,20 @@ public class BaseLogin {
 		driver.manage().window().maximize();
 		
 		driver.get("https://www.zoopla.co.uk/");
+				
+		driver.findElement(By.xpath("//*[@class='ui-button-primary ui-cookie-accept-all-medium-large']")).click();
+					
+		driver.findElement(By.xpath("//*[@class= 'button button--tertiary-dark account-link__text']")).click();
+			
+		driver.findElement(By.xpath("//*[@id='signin_email']")).sendKeys("stevejrnyc@gmail.com");
+		
+		driver.findElement(By.xpath("//*[@id='signin_password']")).sendKeys("smarttech");
+		
+		driver.findElement(By.xpath("//*[@id='signin_submit']")).click();
 		
 		System.out.println(driver.getTitle());
 
+		
 	}
 
 }
